09b036a97bd820ed7bb99e2fee59291c65ba9198
Marta Iachini <iachinimarta@gmail.com>
2022-07-14 16:45:51 +0200

    Modifiche: - Sistemato problema con math random

--- js/main.js
+++ js/main.js
@@ -6,9 +6,14 @@ let bombs = [];
 
 //Genero 16 numeri casuali
 for (let i = 0; i < 16; i++) {
-     let randomNumbers = Math.round(Math.random() * 100);
+    let randomNumbers = Math.floor(Math.random() * 100 + 1);;
 
-     //Inserisco i 16 numeri nell array "bombs"
+    //Imposto il controllo per verificare che non ci siano doppioni nell array
+    while(bombs.includes(randomNumbers)){
+        randomNumbers = Math.floor(Math.random() * 100 + 1);             
+    }
+
+    //Inserisco i 16 numeri nell array "bombs"
     bombs.push(randomNumbers);
 }
 console.log(bombs);
@@ -20,7 +25,6 @@ for (i = 1; i <= 100; i++) {
     let numeri1a100 = i;
     newBoxArray.push(numeri1a100);
 }
-
 console.log(newBoxArray);
 
 
@@ -35,7 +39,7 @@ btn.addEventListener('click',
         container.innerHTML = "";
         
         //Imposto un ciclo for per stampare i box fino a n 100
-        for (let i = 0; i < 100; i++) {
+        for (let i = 1; i <= 100; i++) {
 
             //Creo un div da inserire nel container
             let newBox = document.createElement("div");
@@ -47,7 +51,7 @@ btn.addEventListener('click',
             container.append(newBox);
 
             //Inserisco all interno del box il numero della sua posizione
-            newBox.append(newBoxArray[i]);
+            newBox.append(i);
 
             //Aggiungo un event listener al box 
             newBox.addEventListener('click',
@@ -55,16 +59,26 @@ btn.addEventListener('click',
                     
                     //Imposto una condizione per non stampare due volte lo stesso numero del box nella console.log
                     if (!newBox.classList.contains('bg-yellow')) {
-                        console.log('Hai selezionato il numero: ' + i);
+                        console.log('Hai selezionato il numero: ' + newBoxArray[i]);
                     }
                     
                     //Al click lo sfondo cambia in giallo e stampo il numero del box scelto in console.log
                     //Aggiungo "toggle" = se la classe c'è, la toglie. Se non c'è, la aggiunge.
                     newBox.classList.toggle("bg-yellow");
+
+                    if (newBoxArray.includes(bombs[i])) {
+                        console.log("bomba");
+                    }
                     
                 }
             );
-        }
+        }      
     }
 );
+
+// for (i = 0; i < newBoxArray.length; i++) {
+//     if (bombs.includes(i)) {
+//         console.log(newBoxArray[i], i);
+//     }
+// }
 
